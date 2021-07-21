86ff68eab85a79158ad02d796dd680e9f1b054b8
Elgun Eminov <68548878+elguneminov@users.noreply.github.com>
2021-07-21 20:38:07 +0400

    Add files via upload

---
+++ verilenlerin_tiplerinin_olcusu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"char olcusu: "<<sizeof(char)<<" bayt"<<endl;
+    cout<<"int olcusu: "<<sizeof(int)<<" bayt"<<endl;
+    cout<<"short int olcusu: "<<sizeof(short int)<<" bayt"<<endl;
+    cout<<"long int olcusu: "<<sizeof(long int)<<" bayt"<<endl;
+    cout<<"signed long int olcusu: "<<sizeof(signed long int)<<" bayt"<<endl;
+    cout<<"float olcusu: "<<sizeof(float)<<" bayt"<<endl;
+    cout<<"double olcusu: "<<sizeof(double)<<" bayt"<<endl;
+    cout<<"wchar_t olcusu: "<<sizeof(wchar_t)<<" bayt"<<endl;
+
+    return 0;
+}
